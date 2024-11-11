6667723c282321a5d2d25d71a22e8ba079ecf63e
DrCannata <cannata@utexas.edu>
2024-11-11 01:31:01 +0000

    na

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ st.title("🦜🔗 Quickstart App")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-
 def generate_response(input_text):
     model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
     st.info(model.invoke(input_text))
@@ -16,7 +15,7 @@ import os
 # userpwd = os.environ.get("PYTHON_PASSWORD")
 
 # This was helpful: https://python-oracledb.readthedocs.io/en/latest/user_guide/connection_handling.html
-oracledb. init_oracle_client()
+# oracledb. init_oracle_client()
 connection = oracledb.connect(user="OMLUSER", password="Cenne#e49Cenne#e49",
                               host="150.136.183.171", port=1521, service_name="DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com")
 print(connection)
