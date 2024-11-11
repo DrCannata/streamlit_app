c877bf3d9761fd3b6284b862c051f4ae6ebcf0c0
DrCannata <cannata@utexas.edu>
2024-11-11 17:43:31 +0000

    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,14 @@ import os
 
 # This was helpful: https://python-oracledb.readthedocs.io/en/latest/user_guide/connection_handling.html
 # oracledb.init_oracle_client()
-connection = oracledb.connect(user="OMLUSER", password="Cenne#e49Cenne#e49",
-                              host="150.136.183.171", port=1521, service_name="DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com")
-print(connection)
+# connection = oracledb.connect(user="OMLUSER", password="Cenne#e49Cenne#e49",
+#                               host="150.136.183.171", port=1521, service_name="DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com")
+# print(connection)
 
-with connection.cursor() as cursor:
-    for (r,) in cursor.execute("SELECT VECTOR_EMBEDDING(ALL_MINILM_L12_V2 USING pageContent as data) AS embedding FROM Carnot"):
-        print(r)
-        print("")
+# with connection.cursor() as cursor:
+#     for (r,) in cursor.execute("SELECT VECTOR_EMBEDDING(ALL_MINILM_L12_V2 USING pageContent as data) AS embedding FROM Carnot"):
+#         print(r)
+#        print("")
 
 with st.form("my_form"):
     text = st.text_area(
