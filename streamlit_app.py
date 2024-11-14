04a2d65a1c400ca2ae3c55ab25e7c2c0011cce55
DrCannata <cannata@utexas.edu>
2024-11-14 16:25:02 -0600

    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ import os
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
-        "What are the four key pieces of advice for learning how to code?",
+        "What are the three key pieces of advice for learning how to code?",
     )
     submitted = st.form_submit_button("Submit")
     if not openai_api_key.startswith("sk-"):
