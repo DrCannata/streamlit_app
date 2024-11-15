2892b79d48a5e5f918cf6ac38be43ef97c25f10c
DrCannata <cannata@utexas.edu>
2024-11-15 15:45:20 +0000

    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,6 @@ def generate_response(input_text):
     model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
     st.info(model.invoke(input_text))
 
-import os
-
-
-# userpwd = os.environ.get("PYTHON_PASSWORD")
-
-# This was helpful: https://python-oracledb.readthedocs.io/en/latest/user_guide/connection_handling.html
-# oracledb.init_oracle_client()
-# connection = oracledb.connect(user="OMLUSER", password="Cenne#e49Cenne#e49",
-#                               host="150.136.183.171", port=1521, service_name="DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com")
-# print(connection)
-
-# with connection.cursor() as cursor:
-#     for (r,) in cursor.execute("SELECT VECTOR_EMBEDDING(ALL_MINILM_L12_V2 USING pageContent as data) AS embedding FROM Carnot"):
-#         print(r)
-#        print("")
-
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 /workspaces/sqlcl/bin/sql -S OMLUSER/Cenne#e49Cenne#e49@150.136.183.171/DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com <<!
 SET SQLFORMAT JSON-FORMATTED
+set feedback off
 $1;
 !
