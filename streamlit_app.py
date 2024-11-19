71b405b94a9083391b88574a3f2ca08290c94e80
Phil Cannata <phil.cannata@oracle.com>
2024-11-18 20:36:28 -0600

    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from langchain_openai.chat_models import ChatOpenAI
 st.title("🦜🔗 Quickstart App")
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+subject = st.sidebar.text_input("Query subject")
 
 def generate_response(input_text):
     model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
@@ -12,23 +13,24 @@ def generate_response(input_text):
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
+        # "What are the three key pieces of advice for learning how to code?",
+        "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",
     )
-
-    import subprocess
-    import json
-    result = subprocess.run('"./test.sh" "select \'Quantum Mechanics\' from dual"', shell=True, stdout=subprocess.PIPE)
-    print(result.stdout)
-    json_object = json.loads(result.stdout)
-    print(type(json_object))
-    #print(json_object.get("results".get("items")))
-    my_string = ', '.join(json_object) 
-    print(my_string)
-    text += my_string + " for 2020 election"
-    print(text)
 
     submitted = st.form_submit_button("Submit")
     if not openai_api_key.startswith("sk-"):
         st.warning("Please enter your OpenAI API key!", icon="⚠")
     if submitted and openai_api_key.startswith("sk-"):
+
+        import subprocess
+        import json
+        result = subprocess.run('"./test.sh" "select \'' + subject + '\' as attr from dual"', shell=True, stdout=subprocess.PIPE)
+        # print(result.stdout)
+        json_object = json.loads(result.stdout)
+        for i in json_object.get("results"):
+            # print(i.get("items"))
+            for j in i.get("items"):
+                # print("j is " + str(j))
+                text += j["attr"]
+
         generate_response(text)
