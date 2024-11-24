1f02c738f1e36f32ed81e6c64477e809a695df03
Phil Cannata <phil.cannata@oracle.com>
2024-11-24 12:52:53 -0600

    z

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import subprocess
 import json
 
 #subprocess.call("/workspaces/streamlit_app/test.sh", shell=True)
-result = subprocess.run('"/Users/pcannata/Mine/My Repositories/AI/Python/Streamlit/streamlit_app/test.sh" "select * from dual"', shell=True, stdout=subprocess.PIPE)
+#result = subprocess.run('"/Users/pcannata/Mine/My Repositories/AI/Python/Streamlit/streamlit_app/test.sh" "select * from dual"', shell=True, stdout=subprocess.PIPE)
+result = subprocess.run('"/Users/pcannata/Mine/My Repositories/AI/Python/Streamlit/streamlit_app/test.sh" "select \'First\' as attr union select \'Second\' as attr"', shell=True, stdout=subprocess.PIPE)
 
 # print(result.stdout)
 json_object = json.loads(result.stdout)
--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 SQLQuery"
+	exit 1
+fi
+
 sql -S OMLUSER/Cenne#e49Cenne#e49@150.136.183.171/DB23AI_PDB1.sub08201532330.philfnvcn.oraclevcn.com <<!
 SET SQLFORMAT JSON-FORMATTED
 set feedback off
